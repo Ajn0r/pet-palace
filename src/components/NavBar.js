0526cd4458b01a159794fc905915b0853d640e5b
Ajn0r <ronjasjogren@hotmail.com>
2022-11-05 16:06:01 +0000

    add css files to navbar and app and basic style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../assets/logo.png'
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar 
+    className={styles.NavBar}
+    expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="55"/></Navbar.Brand>
+        <Navbar.Brand 
+          href="#home">
+          <img src={logo} alt="logo" height="75"/>
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Sign in</Nav.Link>
-            <Nav.Link>Sign up</Nav.Link>
+          <Nav className="text-left ml-auto">
+            <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
+            <Nav.Link className={styles.oval}><i className='fas fa-sign-in-alt'></i>Sign in</Nav.Link>
+            <Nav.Link className={styles.oval}><i className='fas fa-user-plus'></i>Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Container>

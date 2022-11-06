fa45bf58b19244db20626e467266d4cc709842de
Ajn0r <ronjasjogren@hotmail.com>
2022-11-06 13:29:22 +0000

    update styles for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,38 @@ import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar 
     className={styles.NavBar}
     expand="md" fixed="top">
-      <Container>
+      <Container fluid>
+        <NavLink to="/">
         <Navbar.Brand 
           href="#home">
           <img src={logo} alt="logo" height="75"/>
-          </Navbar.Brand>
+          </Navbar.Brand></NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="text-left ml-auto">
-            <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
-            <Nav.Link><i className='fas fa-sign-in-alt'></i>Sign in</Nav.Link>
-            <Nav.Link><i className='fas fa-user-plus'></i>Sign up</Nav.Link>
+            <NavLink
+              exact
+              to="/"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              ><i className='fas fa-home'></i>Home</NavLink>
+            <NavLink
+              to="/signin"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              ><i className='fas fa-sign-in-alt'></i>Sign in</NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              ><i className='fas fa-user-plus'></i>Sign up</NavLink>
           </Nav>
         </Navbar.Collapse>
     </Container>

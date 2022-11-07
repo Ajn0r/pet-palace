2c2b845afcd7a3a2390822bcd5ac194811bcdf77
Ajn0r <ronjasjogren@hotmail.com>
2022-11-07 17:18:31 +0000

    add toggler hook and navbar logo, dropdown and media styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import logo from '../assets/logo.png'
-import styles from '../styles/NavBar.module.css'
+import { Container, NavDropdown, Nav, Navbar } from 'react-bootstrap';
+import logo from '../assets/logo.png';
+import smlogo from '../assets/pawlogo.png'
+import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -30,7 +34,7 @@ const NavBar = () => {
       </NavLink>
       {currentUser?.username}
     </>
-  )
+  );
   const loggedOutIcons = (
     <>
       <NavLink
@@ -50,18 +54,57 @@ const NavBar = () => {
       </NavLink>
     </>
   );
+  const newIcon = (
+    <> 
+      <NavDropdown
+        title={
+          <span>
+            <i className='fas fa-plus-circle'></i>
+            New
+          </span>}
+        id="navbarScrollingDropdown"
+        className={`${styles.NewBtn} ml-auto`}
+      >
+        <NavDropdown.Item
+          to="/posts"
+          className={
+            `${styles.NavLink} ${styles.DropDownItem}`}>
+              New post
+        </NavDropdown.Item>
+        <NavDropdown.Item
+          to="/"
+          className={
+            `${styles.NavLink} ${styles.DropDownItem}`}>
+              New ad
+        </NavDropdown.Item>
+      </NavDropdown>
+    </>
+  )
 
   return (
-    <Navbar 
-    className={styles.NavBar}
-    expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md" fixed="top"
+    >
       <Container fluid>
-        <NavLink to="/">
-        <Navbar.Brand 
-          href="#home">
-          <img src={logo} alt="logo" height="70"/>
-          </Navbar.Brand></NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <NavLink to="/"
+          className={styles.LogoLink}>
+          <Navbar.Brand 
+            href="#home"
+            className={styles.Logo}>
+            <img src={logo} alt="logo"
+              className={`${styles.BigLogo} d-none`} />
+            <img src={smlogo} alt="logo"
+              className={`${styles.SmLogo}`}/>
+          </Navbar.Brand>
+        </NavLink>
+        {currentUser && newIcon}
+        <Navbar.Toggle
+          onClick={() => setExpanded(!expanded)}
+          ref={ref}
+          className={styles.Toggle}
+          aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="text-left ml-auto">
             <NavLink
@@ -72,7 +115,7 @@ const NavBar = () => {
               >
                 <i className='fas fa-home'></i>Home
               </NavLink>
-
+              
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>

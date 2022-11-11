70ccfa6dfd0dc311adc9a5035977a3e91e5b86ef
Ajn0r <ronjasjogren@hotmail.com>
2022-11-11 06:53:49 +0000

    Create route for editing posts

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
 import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
+import PostEditForm from './pages/posts/PostEditForm';
 
 function App() {
   const currentUser = useCurrentUser();
@@ -31,6 +32,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts" render={() => (
             <PostsPage
               message="Nothing to see here, try searching for something else or follow a cool cat!"

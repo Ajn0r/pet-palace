b4c57d969085c66d550611b7f88e4d8943862d6f
Ajn0r <ronjasjogren@hotmail.com>
2022-11-13 13:30:44 +0000

    setup for testing

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+const baseURL = "https://pet-palace-api.herokuapp.com/"
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 4,
+        username: "Maja",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 4,
+        profile_image: "https://res.cloudinary.com/majascloud/image/upload/v1/media/images/IMG_4594_ulunjh"
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  })
+];

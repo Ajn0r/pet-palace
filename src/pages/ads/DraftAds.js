b1a8a6beb6cdffd37a408490d7e058c954052642
Ajn0r <ronjasjogren@hotmail.com>
2022-11-17 12:30:56 +0000

    add delete modal to drafts component

--- src/pages/ads/DraftAds.js
+++ src/pages/ads/DraftAds.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 
-
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-import { axiosReq } from "../../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 
-import appStyles from "../../App.module.css"
+import styles from "../../styles/Ad.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css"
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Asset from "../../components/Asset";
 import { Link } from "react-router-dom";
@@ -14,6 +17,7 @@ import { Link } from "react-router-dom";
 const DraftAds = ({mobile}) => {
   const currentUser = useCurrentUser();
   const [ draftData, setDraftData ] = useState({ results: [] });
+  const [ deleted, setDeleted ] = useState(false);
 
   useEffect(() => {
     const handleMount = async () => {
@@ -25,35 +29,80 @@ const DraftAds = ({mobile}) => {
       }
     };
     handleMount();
-  }, [currentUser]);
-  console.log(draftData)
-  console.log(currentUser);
+    setDeleted(false);
+  }, [currentUser, deleted]);
+
+
+  function DeleteModal({id}) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleDelete = async () => {
+      try {
+        await axiosRes.delete(`/ads/${id}`);
+        setDeleted(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    return (
+      <>
+        <i className={`${styles.DraftTrash} far fa-trash-alt ml-auto pr-2`} onClick={handleShow}></i>
+  
+        <Modal show={show} centered onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Delete draft</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>This will delete the Draft</Modal.Body>
+          <Modal.Footer>
+            <Button
+              className={btnStyles.CloseBtn}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+            <Button 
+              className={btnStyles.Button}
+              onClick={handleDelete}>
+              Delete
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
   return (
-    <Container className={`card p-3 mt-4 ${mobile && 'd-lg-none text-center mb-3'}`}>
+    <Container className={`card p-3 mt-4 ${mobile && 'd-lg-none text-center mb-3'} ${!mobile && styles.Sticky}`}>
       {draftData.results.length ? (
         <>
           <i className="fas fa-pen text-center"><span className={`ml-2 ${appStyles.SpanText}`}>Your drafts</span></i>
           <hr className="mx-auto" />
           {mobile ? (
-            <div className='d-flex justify-content-around'>
+            <div className={`${styles.DraftsMobile}`}>
               {draftData.results.slice(0, 4).map((ad) => (
                 <Link
                 to={`/ads/${ad.id}`}
                 key={ad.id}
                 >
-                  <span>Title: {ad.title}</span>
+                  <span className={`${appStyles.SpanText}`}>Title: </span>{ad.title}
                 </Link>
               ))}
             </div>
           ) : (
             draftData?.results.map((ad) => (
-                <Row className="pl-3 mb-1" key={ad.id}>
-                  <Link
-                    to={`/ads/${ad.id}`}
-                    >
-                      <span>Title: {ad.title}</span>
-                    </Link>
-                </Row>
+              <Row className="pl-3 mb-3 align-items-center" key={ad.id}>
+                <Link
+                  to={`/ads/${ad.id}`}
+                  >
+                    <span className={`${appStyles.SpanText}`}>Title: </span>{ad.title}
+                </Link>
+                <DeleteModal id={ad.id}/>
+              </Row>
+
             ))
             )}
         </>

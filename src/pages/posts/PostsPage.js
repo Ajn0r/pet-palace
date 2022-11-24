b6c9c4a9ea28030b1dbf4b5cdc0a94f1256efd84
Ajn0r <ronjasjogren@hotmail.com>
2022-11-24 09:50:16 +0000

    add currentuser to dependency in hook

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,13 +14,14 @@ import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostsPage({ message, filter = '' }) {
   const [ posts, setPosts ] = useState({results: []});
   const [ hasLoaded, setHasLoaded ] = useState(false);
   const { pathname } = useLocation();
-  
   const [ query, setQuery ] = useState("");
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -39,7 +40,7 @@ function PostsPage({ message, filter = '' }) {
     return () => {
       clearTimeout(timer)
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100 pl-sm-3">
